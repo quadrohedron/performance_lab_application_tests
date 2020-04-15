5a1490dc06f8f74d1e7b0c3657d9860e62814072
quadrohedron <quadrohedron@gmail.com>
2020-04-15 19:00:58 +0100

    Create task4.py

---
+++ task4/task4.py
@@ -0,0 +1,62 @@
+import sys
+
+def t_parse(t_string):
+    h, m = map(int, t_string.split(':'))
+    return 60*h+m
+
+def t_format(t_number):
+    h, m = map(str, divmod(t_number, 60))
+    if len(m) == 1:
+        m = '0'+m
+    return h+':'+m
+
+def fetch_input(path):
+    enter_times, exit_times = [], []
+    with open(path) as f:
+        text = f.read()
+    for line in text.strip().split('\n'):
+        a, b = map(t_parse, line.split(' '))
+        enter_times.append(a)
+        exit_times.append(b)
+    return enter_times, exit_times
+
+def find_maxcust_intervals(enter_times, exit_times):
+    temp = {}
+    for t in enter_times:
+        if t in temp:
+            temp[t] += 1
+        else:
+            temp[t] = 1
+    for t in exit_times:
+        if t in temp:
+            temp[t] -= 1
+        else:
+            temp[t] = -1
+    #
+    deltas = {}
+    for t in temp:
+        v = temp[t]
+        if not (v == 0):
+            deltas[t] = v
+    #
+    maxval = 0
+    curval = 0
+    res = []
+    for t, d in sorted(deltas.items(), key = lambda x: x[0]):
+        if d > 0:
+            start_cand = t
+        else:
+            if curval == maxval:
+                res.append((start_cand, t))
+            elif curval > maxval:
+                res = [(start_cand, t)]
+                maxval = curval
+        curval += d
+    #
+    for line in res:
+        print(' '.join(tuple(map(t_format, line))))
+    #
+    return None
+
+if __name__ == '__main__':
+    find_maxcust_intervals(*fetch_input(sys.argv[1]))
